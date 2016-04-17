3bcfea92bc777b0c554ed8737d2212312394230d
c.piotre <c.piotre@gmail.com>
2016-04-17 23:56:44 +0200

    Introduce method variants with executor as param

--- blog-code/src/main/java/pl/ciruk/blog/fanout/CompletableFutures.java
+++ blog-code/src/main/java/pl/ciruk/blog/fanout/CompletableFutures.java
@@ -1,32 +1,34 @@
 package pl.ciruk.blog.fanout;
 
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class CompletableFutures {
-	static <F, T> Function<F, CompletableFuture<Stream<T>>> fanOutAsync(Function<F, T>... mappings) {
-		Function<F, CompletableFuture<Stream<T>>> reduce = input -> Arrays.stream(mappings)
+	static <F, T> Function<F, CompletableFuture<Stream<T>>> fanOut(Collection<Function<F, T>> mappings) {
+		return fanOut(mappings, ForkJoinPool.commonPool());
+	}
+
+	static <F, T> Function<F, CompletableFuture<Stream<T>>> fanOut(Collection<Function<F, T>> mappings, ExecutorService executorService) {
+		Function<F, CompletableFuture<Stream<T>>> reduce = input -> mappings.stream()
 				.map(mapper -> mapper.apply(input)) // from F to T
 				.map(Stream::of) // from T to Stream<T>
 				.map(CompletableFutures::of) // from Stream<T> to CompletableFuture<Stream<T>>
-				.reduce(CompletableFuture.completedFuture(Stream.empty()), combineUsing(Stream::concat));
-		return reduce;
-	}
-
-	static <F, T> Function<F, Stream<T>> fanOut(Function<F, T>... mappings) {
-		Function<F, Stream<T>> reduce = input -> Arrays.stream(mappings)
-				.map(mapper -> mapper.apply(input)) // from F to T
-				.map(Stream::of) // from T to Stream<T>
-				.reduce(Stream.empty(), Stream::concat);
+				.reduce(CompletableFuture.completedFuture(Stream.empty()), combineUsing(Stream::concat, executorService));
 		return reduce;
 	}
 
 	public static <T> BinaryOperator<CompletableFuture<T>> combineUsing(BiFunction<T, T, T> combiningFunction) {
 		return (cf1, cf2) -> cf1.thenCombineAsync(cf2, combiningFunction);
+	}
+
+	public static <T> BinaryOperator<CompletableFuture<T>> combineUsing(BiFunction<T, T, T> combiningFunction, ExecutorService executorService) {
+		return (cf1, cf2) -> cf1.thenCombineAsync(cf2, combiningFunction, executorService);
 	}
 
 	public static <T> CompletableFuture<T> of(T t) {
